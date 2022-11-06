d2f65bebbff41f0f1d5c4903cfb4dffdc475f36c
Eryk <emroczko@me.com>
2022-11-06 22:19:06 +0100

    Refactored main

--- main.py
+++ main.py
@@ -2,117 +2,20 @@ import time
 import torch
 from torch.utils.data import DataLoader, random_split
 from torchtext.data import to_map_style_dataset
-from torchtext.data.utils import get_tokenizer
-from torchtext.vocab import build_vocab_from_iterator
-
 from reviews_dataset import reviews
-from model import TextClassificationModel
+from trainer import Trainer
 
-def train(dataloader):
-    model.train()
-    total_acc, total_count = 0, 0
-    log_interval = 500
-    start_time = time.time()
-
-    for idx, (label, text, offsets) in enumerate(dataloader):
-        optimizer.zero_grad()
-        predicted_label = model(text, offsets)
-        loss = criterion(predicted_label, label)
-        loss.backward()
-        torch.nn.utils.clip_grad_norm_(model.parameters(), 0.1)
-        optimizer.step()
-        total_acc += (predicted_label.argmax(1) == label).sum().item()
-        total_count += label.size(0)
-        if idx % log_interval == 0 and idx > 0:
-            elapsed = time.time() - start_time
-            print('| epoch {:3d} | {:5d}/{:5d} batches '
-                  '| accuracy {:8.3f}'.format(epoch, idx, len(dataloader),
-                                              total_acc / total_count))
-            total_acc, total_count = 0, 0
-            start_time = time.time()
-
-
-def evaluate(dataloader):
-    model.eval()
-    total_acc, total_count = 0, 0
-
-    with torch.no_grad():
-        for idx, (label, text, offsets) in enumerate(dataloader):
-            predicted_label = model(text, offsets)
-            loss = criterion(predicted_label, label)
-            total_acc += (predicted_label.argmax(1) == label).sum().item()
-            total_count += label.size(0)
-    return total_acc / total_count
-
-
-def collate_batch(batch):
-    label_list, text_list, offsets = [], [], [0]
-    for (_label, _text) in batch:
-        label_list.append(label_pipeline(_label))
-        processed_text = torch.tensor(text_pipeline(_text), dtype=torch.int64)
-        text_list.append(processed_text)
-        offsets.append(processed_text.size(0))
-
-    label_list = torch.tensor(label_list, dtype=torch.int64)
-    offsets = torch.tensor(offsets[:-1]).cumsum(dim=0)
-    text_list = torch.cat(text_list)
-    return label_list.to(device), text_list.to(device), offsets.to(device)
-
-
-def yield_tokens(data_iter):
-    for _, text in data_iter:
-        yield tokenizer(text)
-
-
-def predict(text, text_pipeline):
-    with torch.no_grad():
-        text = torch.tensor(text_pipeline(text))
-        output = model(text, torch.tensor([0]))
-        return output.argmax(1).item() + 1
-
+# Hyperparameters
+EPOCHS = 10  # epoch
+LR = 5  # learning rate
+BATCH_SIZE = 64  # batch size for training
 
 if __name__ == "__main__":
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-
     dataset = reviews(split='train')
 
-    # print(next(iter(dataset)))
-    tokenizer = get_tokenizer('spacy', 'pl_core_news_md')
-    # tokenizer = get_tokenizer('basic_english')
+    trainer = Trainer(dataset, device, LR)
 
-    ########################################
-
-    # print(yield_tokens(train_iter))
-    # dataset = AG_NEWS(split='train')
-
-    vocab = build_vocab_from_iterator(yield_tokens(dataset), specials=["<unk>"])
-    vocab.set_default_index(vocab["<unk>"])
-
-    text_pipeline = lambda x: vocab(tokenizer(x))
-    label_pipeline = lambda x: int(x)
-
-    ########################################
-
-    print(text_pipeline("super TV"))
-
-    ########################################
-    num_class = len(set([label for (label, text) in dataset]))
-
-    vocab_size = len(vocab)
-    emsize = 64
-    model = TextClassificationModel(vocab_size, emsize, num_class).to(device)
-
-    print(dataset)
-
-    ########################################
-    # Hyperparameters
-    EPOCHS = 10  # epoch
-    LR = 5  # learning rate
-    BATCH_SIZE = 64  # batch size for training
-
-    criterion = torch.nn.CrossEntropyLoss()
-    optimizer = torch.optim.SGD(model.parameters(), lr=LR)
-    scheduler = torch.optim.lr_scheduler.StepLR(optimizer, 1.0, gamma=0.1)
     total_accu = None
     train_iter, test_iter = reviews()
     train_dataset = to_map_style_dataset(train_iter)
@@ -122,18 +25,18 @@ if __name__ == "__main__":
         random_split(train_dataset, [num_train, len(train_dataset) - num_train])
 
     train_dataloader = DataLoader(split_train_, batch_size=BATCH_SIZE,
-                                  shuffle=True, collate_fn=collate_batch)
+                                  shuffle=True, collate_fn=trainer.collate_batch)
     valid_dataloader = DataLoader(split_valid_, batch_size=BATCH_SIZE,
-                                  shuffle=True, collate_fn=collate_batch)
+                                  shuffle=True, collate_fn=trainer.collate_batch)
     test_dataloader = DataLoader(test_dataset, batch_size=BATCH_SIZE,
-                                 shuffle=True, collate_fn=collate_batch)
+                                 shuffle=True, collate_fn=trainer.collate_batch)
 
     for epoch in range(1, EPOCHS + 1):
         epoch_start_time = time.time()
-        train(train_dataloader)
-        accu_val = evaluate(valid_dataloader)
+        trainer.train(train_dataloader, epoch)
+        accu_val = trainer.evaluate(valid_dataloader)
         if total_accu is not None and total_accu > accu_val:
-            scheduler.step()
+            trainer.scheduler.step()
         else:
             total_accu = accu_val
         print('-' * 59)
@@ -144,17 +47,16 @@ if __name__ == "__main__":
         print('-' * 59)
 
     print('Checking the results of test dataset.')
-    accu_test = evaluate(test_dataloader)
+    accu_test = trainer.evaluate(test_dataloader)
     print('test accuracy {:8.3f}'.format(accu_test))
 
-    ag_news_label = {1: "Negative",
-                     2: "Positive"}
+    reviews_labels = {1: "Negative",
+                      2: "Positive"}
 
     ex_text_str1 = "Super mega telewizor!"
     ex_text_str2 = "beznadziejny, fatalny, żenujący, tragedia, tandeta!"
 
-    model = model.to("cpu")
+    trainer.model = trainer.model.to("cpu")
 
-    print("This is a %s review" % ag_news_label[predict(ex_text_str1, text_pipeline)])
-
-    print("This is a %s review" % ag_news_label[predict(ex_text_str2, text_pipeline)])
+    print("This is a %s review" % reviews_labels[trainer.predict(ex_text_str1)])
+    print("This is a %s review" % reviews_labels[trainer.predict(ex_text_str2)])
