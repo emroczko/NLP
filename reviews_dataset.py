2183dbf01fe2ca2166dd7f08ce6c57a92abba460
Eryk <emroczko@me.com>
2023-01-09 23:49:02 +0100

    Updated dataset function to receive dataset path

--- reviews_dataset.py
+++ reviews_dataset.py
@@ -12,7 +12,7 @@ NUM_LINES = {
 
 @_wrap_split_argument(("train", "test"))
 def reviews(root: str, split: Union[Tuple[str], str]):
-    dp = IterableWrapper(["all_reviews.json"])
+    dp = IterableWrapper([root])
     dp = FileOpener(dp, mode='b')
     dp = dp.parse_json_files()
     return dp.read_dataset().shuffle().set_shuffle(False).sharding_filter()
