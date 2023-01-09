b9ee73ddaa80b292918da67b3bbb905a2f0ea212
Eryk <emroczko@me.com>
2023-01-09 23:48:44 +0100

    Added method to return model

--- main.py
+++ main.py
@@ -11,14 +11,11 @@ EPOCHS = 10  # epoch
 LR = 5  # learning rate
 BATCH_SIZE = 64  # batch size for training
 
-if __name__ == "__main__":
-    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-    dataset = reviews(split='train')
 
-    trainer = Trainer(dataset, device, LR)
+def get_model(trainer: Trainer, dataset_path: str):
 
     total_accu = None
-    train_iter, test_iter = reviews()
+    train_iter, test_iter = reviews(root=dataset_path)
     train_dataset = to_map_style_dataset(train_iter)
     test_dataset = to_map_style_dataset(test_iter)
     num_train = int(len(train_dataset) * 0.95)
@@ -51,6 +48,19 @@ if __name__ == "__main__":
     accu_test = trainer.evaluate(test_dataloader)
     print('test accuracy {:8.3f}'.format(accu_test))
 
+    trainer.model = trainer.model.to("cpu")
+
+    return trainer.model
+
+
+if __name__ == "__main__":
+    dataset_path = 'ProcessedDatasets/all_reviews.json'
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    dataset = reviews(root=dataset_path, split='train')
+
+    trainer = Trainer(dataset, device, LR)
+    trainer.model = get_model(trainer, dataset_path)
+
     reviews_labels = {1: "Negative",
                       2: "Neutral",
                       3: "Positive"}
@@ -58,9 +68,9 @@ if __name__ == "__main__":
     ex_text_str1 = "Super mega proszek, bardzo dobry"
     ex_text_str2 = "Totalny badziew szkoda pieniędzy, beznadziejny, porażka"
     ex_text_str3 = "Nie domywa ale ładnie pachnie. Zostają smugi"
-
-    trainer.model = trainer.model.to("cpu")
+    ex_text_str4 = "Nie rozpuszczają się, dobrze domywają, ładnie pachną, ale bardzo drogie"
 
     print("This is a %s review" % reviews_labels[trainer.predict(ex_text_str1)])
     print("This is a %s review" % reviews_labels[trainer.predict(ex_text_str2)])
     print("This is a %s review" % reviews_labels[trainer.predict(ex_text_str3)])
+    print("This is a %s review" % reviews_labels[trainer.predict(ex_text_str4)])
